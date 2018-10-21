2b82cb42efc58f63a90021d1b61f548064ff7eaf
Vihma <joni.vihmalaakso@gmail.com>
2018-10-21 19:25:22 +0300

    Update Esitehtava_Joni_Vihmalaakso.py

--- Esitehtava_Joni_Vihmalaakso.py
+++ Esitehtava_Joni_Vihmalaakso.py
@@ -1,15 +1,14 @@
-print ("1. Mitä hyötyjä automaattisesta testauksesta on verrattuna manuaaliseen testaukseen?")
-print ("#Automaatinen testaus on huomattavasti nopeampaa ja se vie vähemmän resursseja (työtunnit). Useamman kortin testaus samanaikaisesti on myös mahdollista, toisinkuin manuaalisella testauksella.")
+print("1. Mitä hyötyjä automaattisesta testauksesta on verrattuna manuaaliseen testaukseen?")
+print("#Automaatinen testaus on huomattavasti nopeampaa ja se vie vähemmän resursseja (työtunnit). Useamman kortin testaus samanaikaisesti on myös mahdollista, toisinkuin manuaalisella testauksella.")
 
-print ("2.Mitä haasteita testauksen automatisointi asettaa verrattuna manuaaliseen testaukseen? Pohdi näitä erityisesti kurssin sisällön kontekstissa.")
-print ("##Haasteita on esimerkiksi kuinka luotettavaa testauksen automatisointi on. Löytääkä automatisoitu testaus kaikki virheet, mitä manuaalisessa testauksessa löydettäisiin. Automaattisen testauksen organisointi ja sen pystyttämiseen kuluva aika ja työ.")
+print("2.Mitä haasteita testauksen automatisointi asettaa verrattuna manuaaliseen testaukseen? Pohdi näitä erityisesti kurssin sisällön kontekstissa.")
+print("##Haasteita on esimerkiksi kuinka luotettavaa testauksen automatisointi on. Löytääkä automatisoitu testaus kaikki virheet, mitä manuaalisessa testauksessa löydettäisiin. Automaattisen testauksen organisointi ja sen pystyttämiseen kuluva aika ja työ.")
 
-print ("3.Mihin tarvitaan versionhallintaa? Pohdi asiaa testausprojektin kannalta, jossa on useita rinnakkaisia testaajia tai tiimejä. Minkälaisia haasteita saatat kohdata?")
-print ("###Versionhallintaan tarvitaan, koska rinnakkaiset testaajat tai ryhmät voivat olla niin eri vaiheessa testaustansa, mikä voi sekoittaa onko esimerkiksi uusimmat lisäykset testattu, mitä on otettu mukaan uusimmassa versiossa.")
+print("3.Mihin tarvitaan versionhallintaa? Pohdi asiaa testausprojektin kannalta, jossa on useita rinnakkaisia testaajia tai tiimejä. Minkälaisia haasteita saatat kohdata?")
+print("###Versionhallintaan tarvitaan, koska rinnakkaiset testaajat tai ryhmät voivat olla niin eri vaiheessa testaustansa, mikä voi sekoittaa onko esimerkiksi uusimmat lisäykset testattu, mitä on otettu mukaan uusimmassa versiossa.")
 
-print ("4.Mitä etuja Python tarjoaa kielenä sulautetun järjestelmän testaukseen verrattuna esim. C-kieleen? Mitä haasteita?")
-print ("####Python on aika yksinkertainen kielioppia ja alkaa käyttämään. Haasteita varmasti sulautettuihin järjestelmiin on, että kieli on korkeantason kieli ja esimerkiksi rekistereiden kanssa toimiminen voi olla haaste.")
+print("4.Mitä etuja Python tarjoaa kielenä sulautetun järjestelmän testaukseen verrattuna esim. C-kieleen? Mitä haasteita?")
+print("####Python on aika yksinkertainen kielioppia ja alkaa käyttämään. Haasteita varmasti sulautettuihin järjestelmiin on, että kieli on korkeantason kieli ja esimerkiksi rekistereiden kanssa toimiminen voi olla haaste.")
 
-print ("5.Miten testiautomaatiota voisi soveltaa kurssilla tehtyihin suunnitteluesimerkkeihin?")
- 
-print ("#####Esimerkiksi edellisten tehtävien PWM- tehtävän voisi automatisoida aktivoimaan kytkintä ja seuraamaan jännitearvojen muutosta.")
+print("5.Miten testiautomaatiota voisi soveltaa kurssilla tehtyihin suunnitteluesimerkkeihin?")
+print("#####Esimerkiksi edellisten tehtävien PWM- tehtävän voisi automatisoida aktivoimaan kytkintä ja seuraamaan jännitearvojen muutosta.")
