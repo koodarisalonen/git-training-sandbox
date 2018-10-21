985ff0f1d5e5e690e1ffde4d0628b38ee0618bf1
Kebbo <janne.pih@gmail.com>
2018-10-21 16:01:11 +0300

    Ennakkotehtävän kohta 2

--- Ennakkoteht_JannePihlajaniemi.py
+++ Ennakkoteht_JannePihlajaniemi.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python3.4
 
+qa=[
+	{"question": "Mitä hyötyjä automaattisesta testauksesta on verrattuna manuaaliseen testaukseen?",
+	"answer": "Automaattinen testaus on parempi tapa tehdä triviaaleja toistuvia testejä, joille on selkeä määrittely, sekä pitkäkestoisia testejä kuten toiminnan vakauden testaus."},
+	{"question": "Mitä haasteita testauksen automatisointi asettaa verrattuna manuaaliseen testaukseen?",
+	"answer": "Testiautomaation ylläpito voi käydä rankaksi, jos projektin speksit muuttuvat sen aikana.\n" + 
+	"Se myös sitouttaa kehitystiimin ottamaan testiautomaation huomioon jo kehitysvaiheessa tai osallistumaan ylläpitoon.\n"},
+	{"question": "Mihin tarvitaan versionhallintaa? Pohdi asiaa testausprojektin kannalta, jossa on useita rinnakkaisia testaajia tai tiimejä. Minkälaisia haasteita saatat kohdata?",
+	"answer": "Versionhallinta mahdollistaa koodin osien käyttämisen ristiin projektien ja tiimien välillä, samalla säilyttäen omat kokonaisuudet.\n" + 
+	"Se myös mahdollistaa ajettavan ja kehitettävän koodin pitämisen erillään. TL;DR *Pukeutuu kaapuun ja huutaa: DEVOPS!*"},
+	{"question": "Mitä etuja Python tarjoaa kielenä sulautetun järjestelmän testaukseen verrattuna esim. C-kieleen? Mitä haasteita?",
+	"answer": "Python ja muut scriptauskielet ovat helppo tapa tehdä laajojakin toiminnallisuuksia nopeasti ja ylläpidettävästi.\n" + 
+	"C:llä asioita joutuisi määrittelemään valtavat määrät pienen toiminnallisuudenkin tekemiseksi.\n" +
+	"Toisaalta Pythonilla ei päästä tarkistelemaan asioita suoraan hardware-tasolla(ellei toteuteta jotain rajapintaa erikseen esim. C:llä). "},
+	{"question": "Miten testiautomaatiota voisi soveltaa kurssilla tehtyihin suunnitteluesimerkkeihin?",
+	"answer": "Jos testi on hyvin määritelty, se tarjoaa suoraan pseudokoodin testiscriptin luomiseksi."}
+	]
+
 def main():
-	print("Moro Hervanta!")
+	for row in qa:
+		print("Kysymys {}\n\nVastaus: {} \n".format(row["question"],row["answer"]))
 	
 '''
 Osan 2 vastaukset
@@ -17,7 +35,7 @@ Se myös sitouttaa kehitystiimin ottamaan testiautomaation huomioon jo kehitysva
 Mihin tarvitaan versionhallintaa? Pohdi asiaa testausprojektin kannalta, jossa on useita rinnakkaisia testaajia tai tiimejä. Minkälaisia haasteita saatat kohdata?
 
 Versionhallinta mahdollistaa koodin osien käyttämisen ristiin projektien ja tiimien välillä, samalla säilyttäen omat kokonaisuudet. 
-Se myös mahdollistaa ajettavan ja kehitettävän koodin pitämisen erillään. TL;DR *Pukeutuu kaapuun ja huutaa: "DEVOPS!"* 
+Se myös mahdollistaa ajettavan ja kehitettävän koodin pitämisen erillään. TL;DR *Pukeutuu kaapuun ja huutaa: DEVOPS!* 
 
 Mitä etuja Python tarjoaa kielenä sulautetun järjestelmän testaukseen verrattuna esim. C-kieleen? Mitä haasteita?
 
@@ -28,7 +46,7 @@ Toisaalta Pythonilla ei päästä tarkistelemaan asioita suoraan hardware-tasoll
 Miten testiautomaatiota voisi soveltaa kurssilla tehtyihin suunnitteluesimerkkeihin?
 
 Jos testi on hyvin määritelty, se tarjoaa suoraan pseudokoodin testiscriptin luomiseksi.
- 
+
 '''
 
 if __name__== "__main__":
