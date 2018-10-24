43a45c59ebd0d1ccdbc024448df0901275c087ca
Aleksi-Peltonen <aleksi.peltonen@eng.tamk.fi>
2018-10-24 10:06:38 +0300

    Hello world

---
+++ HelloWorldAleksiPeltonen.py
@@ -0,0 +1,23 @@
+print("Hello World!")
+
+# Vastauksia kysymyksiin:
+
+# "1.	Mitä hyötyjä automaattisesta testauksesta on verrattuna manuaaliseen testaukseen?"
+print("1. Nopeus, tehokkuus, skriptien uudelleenkäyttö, muokattavuus, skaalautuvuus, virheettömyys(jos skriptit kunnossa) ")
+
+"2.	Mitä haasteita testauksen automatisointi asettaa verrattuna manuaaliseen testaukseen? Pohdi näitä erityisesti kurssin sisällön kontekstissa."
+print("2. Testausskriptien pitää olla kunnossa, työkalut voivat olla kalliita ja niissä on omat rajoituksensa. ")
+
+"3.	Mihin tarvitaan versionhallintaa? Pohdi asiaa testausprojektin kannalta, jossa on useita rinnakkaisia testaajia tai tiimejä. Minkälaisia haasteita saatat kohdata?"
+print("3. Se mahdollistaa samanaikaisen tiedostojen käsittelyn, vanhojen versioiden tallettaminen ja niihin palaaminen, varmuuskopiointi, "
+      "muutosten/kehityksen seuraaminen ja ymmärtäminen")
+print("   Haasteina konfliktien selvittäminen, riittävän kommunikoinnin ylläpito ja tehokas organisointi")
+
+"4.	Mitä etuja Python tarjoaa kielenä sulautetun järjestelmän testaukseen verrattuna esim. C-kieleen? Mitä haasteita?"
+print("4. Python on vähemmän virhealtis, luettavampi kieli, nopeampi ja vapaampi kirjoittaa")
+print("   Vastaavasti sen suorituskyky on heikompi, ei sovellu hyvin raskaisiin sovelluksiin ")
+
+"5.	Miten testiautomaatiota voisi soveltaa kurssilla tehtyihin suunnitteluesimerkkeihin?"
+print("5. Voitaisiin tehdä skriptejä jotka testaisivat käyttäjän syötteiden vaikutusta johon manuaalisesti kuluisi kohtuuttoman paljon aikaa ja vaivaa. ")
+
+
